144d93b2baefd8deaf9a92887d0958788f23d0c2
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-14 11:42:48 -0300

    :sparkles: add directions_reduction

---
+++ lib/directions_reduction.ex
@@ -0,0 +1,73 @@
+defmodule DirectionsReduction do
+  @moduledoc """
+  ## Examples
+
+  ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"] -> ["WEST"]
+
+  ["NORTH", "SOUTH", "EAST", "WEST"] -> []
+
+  ["NORTH", "WEST", "SOUTH", "EAST"] -> ["NORTH", "WEST", "SOUTH", "EAST"]
+
+  ~W(SOUTH WEST NORTH NORTH NORTH EAST WEST NORTH NORTH EAST) -> ~W(SOUTH WEST NORTH NORTH NORTH NORTH NORTH EAST)
+
+  ~W(NORTH SOUTH SOUTH EAST WEST NORTH WEST) -> ~W(WEST)
+
+  """
+  def reduce(directions) do
+    # EXAMPLE:
+    # directions = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+
+    initial_acc = []
+
+    # Reduces the given list from the right with a function.
+    List.foldr(directions, initial_acc, fn direction, acc ->
+      direction |> IO.inspect(label: "direction")
+      acc |> IO.inspect(label: "acc")
+      reduce(direction, acc)
+    end)
+  end
+
+  # ****** START ******
+  # 1.
+  # direction: "WEST"
+  # acc: []
+  # do: ["WEST"]
+  defp reduce(direction, [] = _acc), do: [direction] |> IO.inspect(label: "do")
+
+  # ****** END ******
+  # 7.
+  # direction: "NORTH"
+  # acc: ["SOUTH", "WEST"]
+  # do: ["WEST"]
+  defp reduce("NORTH", ["SOUTH" | tail] = _acc), do: tail |> IO.inspect(label: "do")
+
+  # 5.
+  # direction: "SOUTH"
+  # acc: ["NORTH", "WEST"]
+  # do: ["WEST"]
+  defp reduce("SOUTH", ["NORTH" | tail] = _acc), do: tail |> IO.inspect(label: "do")
+
+  defp reduce("WEST", ["EAST" | tail] = _acc), do: tail |> IO.inspect(label: "do")
+
+  # 4.
+  # direction: "EAST"
+  # acc: ["WEST", "NORTH", "WEST"]
+  # do: ["NORTH", "WEST"]
+  defp reduce("EAST", ["WEST" | tail] = _acc), do: tail |> IO.inspect(label: "do")
+
+  # 2.
+  # direction: "NORTH"
+  # acc: ["WEST"]
+  # do: ["NORTH", "WEST"]
+
+  # 3.
+  # direction: "WEST"
+  # acc: ["NORTH", "WEST"]
+  # do: ["WEST", "NORTH", "WEST"]
+
+  # 6.
+  # direction: "SOUTH"
+  # acc: ["WEST"]
+  # do: ["SOUTH", "WEST"]
+  defp reduce(direction, [next | tail] = _acc), do: [direction, next | tail] |> IO.inspect(label: "do")
+end
---
+++ test/directions_reduction_test.exs
@@ -0,0 +1,18 @@
+defmodule DirectionsReductionTest do
+  use ExUnit.Case
+
+  test "#reduce 1" do
+    directions = ~w[NORTH WEST SOUTH EAST]
+    message = "#{directions} cannot be reduced further"
+    assert DirectionsReduction.reduce(directions) == directions, message
+  end
+
+  test "#reduce 2" do
+    directions = ~w[NORTH SOUTH SOUTH EAST WEST NORTH WEST]
+    reduced_directions = ~w[WEST]
+    message = Enum.join(directions, " ") <>
+              " can be reduced to " <>
+              Enum.join(reduced_directions, " ")
+    assert DirectionsReduction.reduce(directions) == reduced_directions, message
+  end
+end
